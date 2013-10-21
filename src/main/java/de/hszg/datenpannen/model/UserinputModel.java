9741e1474667b2050dac54d31141d6779ac16cbd
Paul Weinhold <weinpau@web.de>
2013-10-21 16:47:54 +0200

    Bindings für die UserInputView

--- src/main/java/de/hszg/datenpannen/model/UserinputModel.java
+++ src/main/java/de/hszg/datenpannen/model/UserinputModel.java
@@ -1,7 +1,41 @@
 package de.hszg.datenpannen.model;
 
+import java.util.EnumSet;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SetProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleSetProperty;
+import javafx.collections.FXCollections;
+
 /**
  * Diese Model-Klasse representiert alle Benutzereingaben
  */
 public class UserinputModel {
+
+    private SetProperty<InfluencingFactor> influencingFactorsProperty =
+            new SimpleSetProperty<>(FXCollections.observableSet(EnumSet.noneOf(InfluencingFactor.class)));
+    private IntegerProperty numberOfDatasetProperty = new SimpleIntegerProperty();
+    private ObjectProperty<Sector> sectorProperty = new SimpleObjectProperty<>(Sector.FINANCIAL);
+
+    public SetProperty<InfluencingFactor> influencingFactorsProperty() {
+        return influencingFactorsProperty;
+    }
+
+    public IntegerProperty numberOfDatasetProperty() {
+        return numberOfDatasetProperty;
+    }
+
+    public int getNumberOfDataset() {
+        return numberOfDatasetProperty.get();
+    }
+
+    public void setNumberOfDataset(int number) {
+        numberOfDatasetProperty.set(number);
+    }
+
+    public ObjectProperty<Sector> sectorProperty() {
+        return sectorProperty;
+    }
 }
--- src/main/java/de/hszg/datenpannen/view/userinput/UserinputPresenter.java
+++ src/main/java/de/hszg/datenpannen/view/userinput/UserinputPresenter.java
@@ -1,17 +1,62 @@
 package de.hszg.datenpannen.view.userinput;
 
-import javafx.fxml.Initializable;
-
+import de.hszg.datenpannen.model.InfluencingFactor;
+import de.hszg.datenpannen.model.Sector;
+import de.hszg.datenpannen.model.UserinputModel;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.util.converter.NumberStringConverter;
+import javax.inject.Inject;
 
 /**
- * Presenter für den Bereich der Nutzereingaben. Hier werden
- * die Input-Elemente mit dem ViewModel verbunden.
+ * Presenter für den Bereich der Nutzereingaben. Hier werden die Input-Elemente
+ * mit dem ViewModel verbunden.
  */
-public class UserinputPresenter implements Initializable{
+public class UserinputPresenter implements Initializable {
+
+    @FXML
+    private VBox influencingFactorBox;
+    @FXML
+    private TextField numberOfDatasetsField;
+    @FXML
+    private ChoiceBox<Sector> sectorChoiceBox;
+    @Inject
+    private UserinputModel userinputModel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("UserinputPresenter initialized");
+
+        numberOfDatasetsField.textProperty().bindBidirectional(userinputModel.numberOfDatasetProperty(), new NumberStringConverter());
+
+        sectorChoiceBox.getItems().addAll(Sector.values());
+        sectorChoiceBox.valueProperty().bindBidirectional(userinputModel.sectorProperty());
+
+        initInflueningFactorCheckBoxes();
+    }
+
+    private void initInflueningFactorCheckBoxes() {
+        for (final InfluencingFactor factor : InfluencingFactor.values()) {
+            CheckBox checkbox = new CheckBox(factor.name());
+            checkbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                @Override
+                public void changed(ObservableValue<? extends Boolean> ov, Boolean t, Boolean selected) {
+                    if (selected) {
+                        userinputModel.influencingFactorsProperty().add(factor);
+                    } else {
+                        userinputModel.influencingFactorsProperty().remove(factor);
+                    }
+                }
+            });
+            influencingFactorBox.getChildren().add(checkbox);
+        }
     }
 }
